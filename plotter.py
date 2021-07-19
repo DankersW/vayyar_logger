33e22dd5c0bc15519c81165f49d542f7c1a953bd
DankersW <wouter.dankers@skynet.be>
2021-07-19 13:18:23 +0200

    db: creating a class for db actions

--- plotter.py
+++ plotter.py
@@ -2,38 +2,45 @@ import boto3
 from boto3.dynamodb.conditions import Key
 from yaml import safe_load
 import time
-import datetime
+
+#from matplotlib import pyplot
 
 
-def query_db(oldest_timestamp, untill_timestamp=None):
-    db = session.resource('dynamodb')
-    db_table = db.Table('vayyar_home_c2c_room_status')
-    if untill_timestamp:
-        query = Key('device_id').eq('id_MzA6QUU6QTQ6RTQ6MDA6NTQ') & Key('timestamp').between(oldest_timestamp, untill_timestamp)
-    else:
-        #query = Key('device_id').eq('id_MzA6QUU6QTQ6RTQ6MDA6NTQ') & Key('timestamp').gte(oldest_timestamp)
-        query = Key('device_id').eq('id_MzA6QUU6QTQ6RTQ6MDA6NTQ') & Key('timestamp').between(oldest_timestamp, 1626687473931)
-    response = db_table.query(KeyConditionExpression=query)
-    return response['Items']
+class DbDriver:
+    def __init__(self, table: str):
+        session = boto3.Session(
+            aws_access_key_id=config.get("aws_access_key"),  # named tuple settings
+            aws_secret_access_key=config.get("aws_secret_key"),
+        )
+        db = session.resource('dynamodb')
+        self.db_table = db.Table(table)
+
+    def query_db(self, oldest_timestamp, untill_timestamp=None):
+        if untill_timestamp:
+            query = Key('device_id').eq('id_MzA6QUU6QTQ6RTQ6MDA6NTQ') & Key('timestamp').between(oldest_timestamp,
+                                                                                                 untill_timestamp)
+        else:
+            # query = Key('device_id').eq('id_MzA6QUU6QTQ6RTQ6MDA6NTQ') & Key('timestamp').gte(oldest_timestamp)
+            query = Key('device_id').eq('id_MzA6QUU6QTQ6RTQ6MDA6NTQ') & Key('timestamp').between(oldest_timestamp,
+                                                                                                 1626687473931)
+        return self.db_table.query(KeyConditionExpression=query)['Items']
+
+    def scan_db(self):
+        return self.db_table.scan()['Items']
+
 
 
 with open("keys.yml") as config_file:
     config = safe_load(config_file)
 
-session = boto3.Session(
-    aws_access_key_id=config.get("aws_access_key"),  # named tuple settings
-    aws_secret_access_key=config.get("aws_secret_key"),
-)
-#dynamodb = session.resource('dynamodb')
-#table = dynamodb.Table('vayyar_home_c2c_room_status')
-#resp = table.scan()
-#data = resp['Items']
-
 start_timestamp = round(time.time() * 1000)
 start_timestamp = 1626686100002
 timestamp_yesterday = start_timestamp - 86400000
-live_data = query_db(oldest_timestamp=start_timestamp)
-yesterday_data = query_db(oldest_timestamp=timestamp_yesterday, untill_timestamp=start_timestamp)
+
+db = DbDriver(table='vayyar_home_c2c_room_status')
+
+live_data = db.query_db(oldest_timestamp=start_timestamp)
+yesterday_data = db.query_db(oldest_timestamp=timestamp_yesterday, untill_timestamp=start_timestamp)
 
 for i in live_data:
     print(i)
@@ -43,8 +50,6 @@ for i in yesterday_data:
 
 # dynamodb = boto3.resource('dynamodb', region_name='***')
 
-# todo: query one day of data
-# todo: query every item more than the start time
 # todo: live plot
 # todo: day plot
 
