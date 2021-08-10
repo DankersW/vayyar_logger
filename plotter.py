6751c7477632fbe2547e720f224dad3c3c273754
DankersW <wouter.dankers@skynet.be>
2021-08-10 13:39:16 +0200

    data: combining data tables

--- plotter.py
+++ plotter.py
@@ -2,6 +2,8 @@ from yaml import safe_load
 from time import time
 from datetime import datetime
 from typing import Tuple
+from decimal import Decimal
+from operator import itemgetter
 
 from matplotlib import pyplot, animation
 
@@ -11,6 +13,7 @@ from db_driver import DbDriver
 class Plotter:
     def __init__(self):
         self.db_room = DbDriver(keys=self._get_keys(), table='vayyar_home_c2c_room_status')
+        self.db_fall = DbDriver(keys=self._get_keys(), table='vayyar_home_c2c_fall_data')
 
         self.start_timestamp = round(time() * 1000)
 
@@ -18,9 +21,9 @@ class Plotter:
         self.live_plot = fig.add_subplot(2, 2, 1)
         self.live_table = fig.add_subplot(2, 2, 2)
         self.yesterday_plot = fig.add_subplot(2, 2, 3)
-        #self.fall_table = fig.add_subplot(2, 2, 4)
 
-        _ = animation.FuncAnimation(fig, self.plot_live, interval=30000)
+        #_ = animation.FuncAnimation(fig, self.plot_live, interval=30000)
+        _ = animation.FuncAnimation(fig, self.plot_live, interval=5000)
         self.plot_yesterday_room_occupation()
 
         pyplot.setp(self.live_plot.get_xticklabels(), rotation=30, ha='right')
@@ -34,25 +37,33 @@ class Plotter:
 
     def plot_live(self, _):
         #data = self.db_room.query_db(oldest_timestamp=self.start_timestamp)
-        data = self.db_room.query_db(oldest_timestamp=1628586788117)
-        self._plot_data(sub_plot=self.live_plot, data=data)
-        self.plot_table(data=data)
+        presence_data = self.db_room.query_db(oldest_timestamp=1628586788117)
+        fall_data = self.db_fall.query_db(oldest_timestamp=1628586788117)
+        self._plot_data(sub_plot=self.live_plot, data=presence_data)
+        self.plot_table(presence_data=presence_data, fall_data=fall_data)
         self.live_plot.set_title("Live monitoring")
 
-    def plot_table(self, data):
-        if not data:
+    def plot_table(self, presence_data, fall_data):
+        if not presence_data and not fall_data:
             return
         table_data = []
+        data = []
         prev_room_status = None
-        for item in data:
+        for item in presence_data:
             if item.get('room_occupied') != prev_room_status:
                 dt, room_occupied = self._parse_data_entry(entry=item)
                 li = [dt, room_occupied]
                 table_data.append(li)
                 prev_room_status = item.get('room_occupied')
+                data.append(item)
+        data += fall_data
+        sorted_events = sorted(data, key=itemgetter('timestamp'))
+        print(sorted_events)
+
+        exit(1)
         self.live_table.axis('tight')
         self.live_table.axis('off')
-        self.live_table.table(cellText=table_data, colLabels=["timestamp", "room_occupied"])
+        self.live_table.table(cellText=table_data, colLabels=["timestamp", "event"])
 
     def plot_yesterday_room_occupation(self):
         timestamp_yesterday = self.start_timestamp - 86400000
@@ -69,12 +80,14 @@ class Plotter:
             y.append(room_occupied)
         sub_plot.step(x, y)
 
-    @staticmethod
-    def _parse_data_entry(entry: dict) -> Tuple[datetime, int]:
-        timestamp = entry.get("timestamp").__int__() / 1000
-        dt = datetime.fromtimestamp(timestamp)
+    def _parse_data_entry(self, entry: dict) -> Tuple[datetime, int]:
+        dt = self._decimal_timestamp_to_dt(timestamp=entry.get("timestamp"))
         occupied = 1 if entry.get("room_occupied") else 0
         return dt, occupied
+
+    @staticmethod
+    def _decimal_timestamp_to_dt(timestamp: Decimal) -> datetime:
+        return datetime.fromtimestamp(timestamp.__int__() / 1000)
 
 
 if __name__ == '__main__':
