3e74961f41271307e0186c0eb71b590ee08b8a8e
DankersW <wouter.dankers@skynet.be>
2021-10-26 10:47:52 +0200

    data: processing of new data format

--- plotter.py
+++ plotter.py
@@ -6,5 +6,5 @@ from weldcloud_overlay import WeldcloudOverlay
 if __name__ == '__main__':
     weldcloud_data = Path("data/Weldcloud_Export_Weldsessions.xlsx")
 
-    LivePlotter()
-    #WeldcloudOverlay(filename=weldcloud_data)
+    #LivePlotter()
+    WeldcloudOverlay(filename=weldcloud_data)
